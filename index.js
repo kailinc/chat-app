78f0c5081599c79d92fd0ae678a16b1c910c0bc0
kailinc <kailinc@bu.edu>
2017-08-14 13:04:09 -0400

    Added Feature to send message and receive message

--- index.js
+++ index.js
@@ -21,5 +21,16 @@ let io = socket(server)
 io.on('connection', (socket) => {
   // socket is the particular socket made in that connection
   console.log('made socket connection')
+  console.log(socket.id)
+
+  socket.on('chat', function (data) {
+    // referring the that particular socket
+
+    io.sockets.emit('chat', data)
+    // refer to all the socket connected to the server
+    // send the data to all socket connected to the server
+  })
+  // socket that is connected
+
 })
 // listen for connection with browser
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,23 @@
-// make a connection
-// socket for the front end
-let socket = io.connect('https://localhost:4000')
+// Make connection
+let socket = io.connect('http://localhost:4000');
+
+
+// send chat message
+$('#send').on('submit', function (e) {
+  e.preventDefault()
+  // getting value of the forms
+  let message = $('#text').val()
+  let handle = $('#handle').val()
+
+  socket.emit('chat', {
+    message: message,
+    handle: handle
+  })
+  // send the message down the socket
+})
+
+// listen for events
+
+socket.on('chat', function(data) {
+  $('#output').append('<p><strong>' + data.handle + ': </strong> ' + data.message + '</p>')
+})
