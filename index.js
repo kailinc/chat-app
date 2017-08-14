8aa74ec5f62552316fee50faf05c517ed10f1f21
kailinc <kailinc@bu.edu>
2017-08-14 11:16:43 -0400

    Set up Socket.io on front end and back end

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 let express = require('express')
+
+let socket = require('socket.io')
+
+// App Set up
+let app = express()
+
+let server = app.listen(4000, function () {
+  console.log('Listening for requests on port 4000')
+})
+// will listen to the port
+
+// static files to show when people on the site
+// will look for index.html to show
+app.use(express.static('public'))
+
+// setting up socket
+
+let io = socket(server)
+
+io.on('connection', (socket) => {
+  // socket is the particular socket made in that connection
+  console.log('made socket connection')
+})
+// listen for connection with browser
---
+++ public/chat.js
@@ -0,0 +1,3 @@
+// make a connection
+// socket for the front end
+let socket = io.connect('https://localhost:4000')
