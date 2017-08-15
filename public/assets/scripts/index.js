74f9c8d1b77626c1c6a5f9f9f17c4fdc1661f74a
kailinc <kailinc@bu.edu>
2017-08-14 21:44:21 -0400

    Initialized HTML for Chat Rooms
    - created form for creating a chat room

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+  // $('.joinRoom').on('click', function () {
+  //   console.log('welcome')
+  // })
+  $('#createRoom').on('click', function (e) {
+    e.preventDefault()
+    console.log('creating room')
+  })
+})
